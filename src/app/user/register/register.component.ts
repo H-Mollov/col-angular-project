54db070a3fbcef8423a019d10768efd9738738eb
H.Mollov <hasan.mollov93@gmail.com>
2020-12-07 12:10:37 +0200

    Configured and tested User Registration with back-end REST API

--- src/app/user/register/register.component.ts
+++ src/app/user/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { UserService } from '../user.service'
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -7,12 +9,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private user: UserService,
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
   }
 
   formHandler(formData: {username: string, password: string, repeatPassword: string}) {
-    
+    this.user.register(formData).subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+          this.router.navigate([`/user/login`]);
+        },
+        error: (err) => {
+          console.error(err);
+        }
+      }
+    )
   }
 }
--- src/app/user/user.service.ts
+++ src/app/user/user.service.ts
@@ -16,26 +16,26 @@ export class UserService {
   currentUser = this._currentUser.asObservable();
 
   apiUrl: string = environment.apiURL;
-  endPoint = environment.endPoint.user;
+  user = environment.endPoint.user;
 
   constructor(
     private http: HttpClient
   ) { }
 
   login(userData: any): Observable<any> {
-    return this.http.post(this.endPoint.login, userData).pipe(
+    return this.http.post(`${this.apiUrl}${this.user.login}`, userData).pipe(
       tap((user: any) => this._currentUser.next(user)),
     )
   }
 
   register(userData: any): Observable<any> {
-    return this.http.post(this.endPoint.register, userData).pipe(
+    return this.http.post(`${this.apiUrl}${this.user.register}`, userData).pipe(
       tap((user: any) => this._currentUser.next(user)),
     )
   }
 
   logout(): Observable<any> {
-    return this.http.get(this.endPoint.logout, {
+    return this.http.get(`${this.apiUrl}${this.user.logout}`, {
       headers: {['user-token']: `USER TOKEN SHOULD BE PUT HERE`}
     }).pipe(
       tap((user: any) => this._currentUser.next(null))
