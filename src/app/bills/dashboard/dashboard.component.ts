13d1e4a7a7e7e5f27da74e457f1642e8cc86f403
H.Mollov <hasan.mollov93@gmail.com>
2020-12-10 22:53:44 +0200

    Integrated Bills Dashboard with Back-End API functionallity

--- src/app/bills/dashboard/dashboard.component.ts
+++ src/app/bills/dashboard/dashboard.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { BillsService } from '../bills.service';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,9 +8,53 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DashboardComponent implements OnInit {
 
-  constructor() { }
+  allBills: any;
+
+  currentMonthBills: any[] = [];
+  currentMonthBillsTotalAmount: number = 0;
+
+  currentUsersList: string[] = [];
+  currentUsersBillInformation: any[] = [];
+
+  constructor(
+    private billsService: BillsService
+  ) { }
 
   ngOnInit(): void {
-  }
+    this.billsService.getAllBills().subscribe(allBills => {
+      this.allBills = allBills;
+      const currentMonth = new Date().getUTCMonth();
 
+      this.currentMonthBills = this.allBills.filter((bills) => bills.month === currentMonth);
+      this.currentMonthBillsTotalAmount = 0;
+
+      this.currentMonthBills.forEach((bill) => {
+        this.currentMonthBillsTotalAmount += bill.owedAmountTOTAL;
+
+        const billPayers = JSON.parse(bill.owedBy);
+
+      billPayers.forEach((payer) => {
+
+        const index = this.currentUsersList.indexOf(payer.name);
+
+        if (index === -1) {
+          this.currentUsersList.push(payer.name);
+          this.currentUsersBillInformation.push({
+            name: payer.name,
+            bills: [{
+              name: bill.name,
+              amount: payer.amount
+            }]
+          })
+        } else {
+          const currentPayer = this.currentUsersBillInformation[index];
+          currentPayer.bills.push({
+            name: bill.name,
+            amount: payer.amount
+          })
+        }
+      })
+      })
+    });
+  }
 }
